6f9e302f7f3589993a7f47bf7629a92cc1939d8a
naiemhosen430 <naiemhosen430@gmail.com>
2024-07-14 09:14:25 +0600

    d

--- src/utils/Authentication.js
+++ src/utils/Authentication.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 export const authentication = (req, res, next) => {
   try {
-    const token = req.headers["authorization"].split(" ")[1];
+    const token = req.headers["authorization"]?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({
