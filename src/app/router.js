89a987b21731c683884694b34d2db61d6124acc9
MD Naiem Hosen <109727997+naiemhosen430@users.noreply.github.com>
2024-07-27 14:09:01 +0600

    Update router.js

--- src/app/router.js
+++ src/app/router.js
@@ -14,11 +14,11 @@ router.get("/", (req, res) => {
 });
 
 // user router
-router.use("/api/v1/auth", authRouter);
-router.use("/api/v1/user", userRouter);
-router.use("/api/v1/post", postRouter);
-router.use("/api/v1/chat", chatRouter);
-router.use("/api/v1/search", searchRouter);
-router.use("/api/v1/ntf", ntfRouter);
+router.use("/api/v3/auth", authRouter);
+router.use("/api/v3/user", userRouter);
+router.use("/api/v3/post", postRouter);
+router.use("/api/v3/chat", chatRouter);
+router.use("/api/v3/search", searchRouter);
+router.use("/api/v3/ntf", ntfRouter);
 
 export default router;
